68279767ae04e0b673258a50c06fe5321e30f473
AbhishekMankame <abhishekmankame962@gmail.com>
2023-03-22 14:41:59 +0530

    data_comment_update

--- stl/vector/intro.cpp
+++ stl/vector/intro.cpp
@@ -1,7 +1,8 @@
 /*
 Vector:
 Vectors are sequence containers representing arrays that can change in size.
-
+Just like arrays, vectors use contiguous storage locations for their elements, which means that their elements can also be accessed directly in O(1) time.
+But unlike arrays, their size can change dynamically, with their storage begin handled automatically by the container.
 
 */
 
