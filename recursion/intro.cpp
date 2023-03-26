6e6dee38ea7cc261e8d499f05931b16137d6d1b2
AbhishekMankame <abhishekmankame962@gmail.com>
2023-03-26 19:30:40 +0530

    simple_update

--- recursion/intro.cpp
+++ recursion/intro.cpp
@@ -14,7 +14,7 @@ void func(int n){
     if(n==0) return;
     cout<<n<<endl; //Here func(5) is called at first
     func(n-1);
-    cout<<n<<endl;
+    cout<<n<<endl; //Here func(1) will be considered first
 
 }
 
