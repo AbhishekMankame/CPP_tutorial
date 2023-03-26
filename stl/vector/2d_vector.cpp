a5238633dc973a3756c8b59cec70edeb9f06c49c
AbhishekMankame <abhishekmankame962@gmail.com>
2023-03-26 09:55:21 +0530

    2d_vector_update

--- stl/vector/2d_vector.cpp
+++ stl/vector/2d_vector.cpp
@@ -8,4 +8,9 @@ int main(){
     freopen("output.txt","w",stdout);
     freopen("error.txt","w",stderr);
 #endif
+
+//2D vector
+    vector<vector<int>> arr={
+        {1,2,3},{4,5,6},{7,8,9,10},{11,12}
+    };
 }
