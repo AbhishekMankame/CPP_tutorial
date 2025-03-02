450ecece415242f47d3190eb55347c510b5a6b35
abhishek <abhishekmankame962@gmail.com>
2025-03-02 22:32:25 +0530

    simple_update

--- old_data/patterns_problem/patterns_1.cpp
+++ old_data/patterns_problem/patterns_1.cpp
@@ -22,7 +22,7 @@ void fast(){
 void solve(){
     int n;
     cin>>n;
-    for(int i=0;i<n;i++){
+/*    for(int i=0;i<n;i++){
         for(int j=0;j<i;j++){
             cout<<" ";
         }
@@ -30,6 +30,11 @@ void solve(){
             cout<<i+1;
         }
         cout<<endl;
+     } */
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-i-1;j++){
+            cout<<" ";
+        }
     }
 
 }
