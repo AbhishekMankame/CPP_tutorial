02a766b36588f2e7f3d9c62625156d2af1a1c77e
abhishek <abhishekmankame962@gmail.com>
2025-03-02 22:42:34 +0530

    simple_update

--- old_data/patterns_problem/patterns_1.cpp
+++ old_data/patterns_problem/patterns_1.cpp
@@ -35,10 +35,10 @@ void solve(){
         for(int j=0;j<n-i-1;j++){
             cout<<" ";
         }
-        for(int j=1;j<i+1;j++){
+        for(int j=1;j<=i+1;j++){
             cout<<j;
         }
-        for(int j=i;j>=0;j--){
+        for(int j=i;j>=1;j--){
             cout<<j;
         }
         cout<<endl;
