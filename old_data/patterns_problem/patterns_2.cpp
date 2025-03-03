b5c48fd06d369047965931c0756c73ab6cef21a8
abhishek <abhishekmankame962@gmail.com>
2025-03-03 13:07:53 +0530

    diamond_top_update

--- old_data/patterns_problem/patterns_2.cpp
+++ old_data/patterns_problem/patterns_2.cpp
@@ -13,8 +13,17 @@ void solve(){
 
     for(int i=0;i<n;i++){
         for(int j=0;j<n-i-1;j++){
-            cout<<"*";
+            cout<<" ";
         }
+        cout<<"*";
+        if(i!=0){
+            for(int j=0;j<2*i-1;j++){
+                cout<<" ";
+            }
+            cout<<"*";
+
+        }
+        
         cout<<endl;
     }
 
