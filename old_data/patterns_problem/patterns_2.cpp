74909615a43239f845003ce216e5e903012f5ecb
abhishek <abhishekmankame962@gmail.com>
2025-03-03 13:21:14 +0530

    full_hollow_diamond_code_update

--- old_data/patterns_problem/patterns_2.cpp
+++ old_data/patterns_problem/patterns_2.cpp
@@ -27,6 +27,23 @@ void solve(){
         cout<<endl;
     }
 
+    // for bottom part
+    for(int i=0;i<n-1;i++){
+        //spaces
+        for(int j=0;j<i+1;j++){
+            cout<<" ";
+        }
+        cout<<"*";
+        if(i!=n-2){
+            //spaces
+            for(int j=0;j<2*(n-i)-5;j++){
+                cout<<" ";
+            }
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+
 }
 
 int main(){
