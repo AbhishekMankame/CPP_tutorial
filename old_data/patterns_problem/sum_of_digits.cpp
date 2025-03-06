fae9c3ec77570ae70c4c22f6a52521def27b73ba
abhishek <abhishekmankame962@gmail.com>
2025-03-06 10:28:03 +0530

    sum_of_digits_update

--- old_data/patterns_problem/sum_of_digits.cpp
+++ old_data/patterns_problem/sum_of_digits.cpp
@@ -7,7 +7,20 @@ void fast(){
     cout.tie(0);
 }
 
+int sumOfDigits(int n){
+    int sum=0;
+    while(n>0){
+        sum=sum+n%10;
+        n=n/10;
+    }
+    return sum;
+}
+
 void solve(){
+    // Here we will calculate the sum of digits of a number using functions
+    int n;
+    cin>>n;
+    cout<<sumOfDigits(n)<<endl;
 
 }
 
