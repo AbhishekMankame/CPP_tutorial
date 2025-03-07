e92ac13b3ae2d1e0edb779e6080d29fee01313d7
abhishek <abhishekmankame962@gmail.com>
2025-03-07 10:35:16 +0530

    dec_to_bin_update

---
+++ old_data/patterns_problem/decimal_to_binary.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void fast(){
+    ios_base::sync_with_stdio(0); 
+    cin.tie(0); 
+    cout.tie(0);
+}
+
+int decToBin(int decNum){
+    int ans=0;
+    int pow=1;
+    int rem;
+    while(decNum>0){
+        rem=decNum%2;
+        decNum/=2;
+        ans=ans+rem*pow;
+        pow=pow*10;
+    }
+    return ans;
+}
+
+void solve(){
+    int decNum=6;
+    cout<<decToBin(decNum)<<endl;
+    
+    
+
+}
+
+int main(){
+#ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    freopen("error.txt","w",stderr);
+#endif
+
+    fast();
+    solve();
+
+    
+    return 0;
+}
