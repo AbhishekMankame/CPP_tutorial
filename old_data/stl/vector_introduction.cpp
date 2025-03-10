ae2c4752fb5986017ece5805ba99ef5806962647
abhishek <abhishekmankame962@gmail.com>
2025-03-10 06:12:53 +0530

    simple_update

--- old_data/stl/vector_introduction.cpp
+++ old_data/stl/vector_introduction.cpp
@@ -5,5 +5,10 @@ using namespace std;
 int main(){
     vector<int> v = {1, 2, 3};
     cout << v[0] << endl;
+
+    vector<int> v2(3, 50);
+    for(int i : v2){
+        cout << i << " ";
+    }
     return 0;
 }
