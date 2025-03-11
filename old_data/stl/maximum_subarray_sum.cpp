28a783593b72b931a2075f0c2773c117ff7785b3
abhishek <abhishekmankame962@gmail.com>
2025-03-11 11:38:00 +0530

    max_subarrary_sum_brute_force_approach

--- old_data/stl/maximum_subarray_sum.cpp
+++ old_data/stl/maximum_subarray_sum.cpp
@@ -2,6 +2,21 @@
 #include<vector>
 using namespace std;
 
+
 int main(){
-    
+    int arr[5]={2,4,-5,3,6};
+    int size=5;
+    int maxSum=INT_MIN;
+    //int curSum=0;
+
+    for(int start=0;start<size;start++){
+        int curSum=0;
+        for(int end=start;end<size;end++){
+            curSum+=arr[end];
+            maxSum=max(maxSum,curSum);
+            //cout<<curSum<<" ";
+        }
+    }
+    cout<<maxSum<<endl;
+
 }
