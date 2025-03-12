9207c31481d2e72739964bbe4553128203330bd3
abhishek <abhishekmankame962@gmail.com>
2025-03-12 09:44:51 +0530

    pair_sum_brute_force_approach_using_function

--- old_data/stl/pairsum.cpp
+++ old_data/stl/pairsum.cpp
@@ -2,19 +2,29 @@
 #include<vector>
 using namespace std;
 
+vector<int> pairSum(vector<int> nums, int target){
+    //Brute Force Approach where time complexity will be O(n^2)
+
+    vector<int> result;
+    int n=nums.size();
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            if(nums[i]+nums[j]==target){
+                result.push_back(i);
+                result.push_back(j);
+                
+            }
+        }
+    }
+    return result;
+}
+
 int main(){
     vector<int> nums={2,7,11,15};
     int n=nums.size();
     int target=9;
-    vector<int> result;
+    vector<int> result=pairSum(nums,target);
+    cout<<result[0]<<","<<result[1]<<endl;;
 
-    //Brute Force Approach where time complexity will be O(n^2)
-    for(int i=0;i<n;i++){
-        for(int j=i+1;j<n;j++){
-            if(nums[i]+nums[j]==target){
-                cout<<i<<" "<<j<<endl;
-            }
-        }
-    }
-
+    return 0;
 }
