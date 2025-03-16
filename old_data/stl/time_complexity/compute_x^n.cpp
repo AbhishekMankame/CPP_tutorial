4fee7ec19a9adb6bc35b812dbd3576983bcf336a
abhishek <abhishekmankame962@gmail.com>
2025-03-16 21:56:37 +0530

    cumpute_x^n_info_update

---
+++ old_data/stl/time_complexity/compute_x^n.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+/*Computing x<n
+In Leet code, question number 50, Pow(x,n) is a famous question. In this question, we are give two integers x and n.
+We have to compute x^n. The naive approach is to multiply x n times. But this approach is not efficient.
+
+*/
+
+int main(){
+
+
+    return 0;
+}
