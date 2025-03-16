3f6f53188de6fab826f21559816a35c589a96a1e
abhishek <abhishekmankame962@gmail.com>
2025-03-16 20:29:31 +0530

    linear_search

--- old_data/stl/time_complexity/introduction.cpp
+++ old_data/stl/time_complexity/introduction.cpp
@@ -17,5 +17,19 @@ using namespace std;
 int main(){
 
 
+    // Linear Search --> O(N) complexity
+    vector<int> v = {1, 2, 3, 4, 5};
+    int x=4;
+
+    for(int i=0;i<v.size();i++){
+        if(v[i]==x){
+            cout<<"Element "<<x<<" is found at the location "<<i;
+            break;
+        }
+        //cout<<endl;
+    }
+    cout<<endl;
+
+
     return 0;
 }
