b191e95a9b881cd0b5fe54e13ef471d1c2f241f8
abhishek <abhishekmankame962@gmail.com>
2025-03-17 12:33:31 +0530

    stock_code_update

--- old_data/stl/time_complexity/stock_buy_and_sell1.cpp
+++ old_data/stl/time_complexity/stock_buy_and_sell1.cpp
@@ -13,3 +13,25 @@ Constraints:
 2. 0<=prices[i]<=10^4
 
 */
+
+// Let's code it!!!!!
+
+int maxProfit(vector<int>& prices){
+    int maxP=0, bestBuy=prices[0];
+
+    for(int i=0;i<prices.size();i++){
+        if(prices[i]>bestBuy){
+            maxP=max(maxP, prices[i]-bestBuy);
+        }
+        bestBuy=min(bestBuy, prices[i]);
+    }
+
+    return maxP;
+}
+
+int main(){
+    vector<int> prices={7,1,5,3,6,4};
+    cout<<maxProfit(prices)<<endl;
+
+    return 0;
+}
