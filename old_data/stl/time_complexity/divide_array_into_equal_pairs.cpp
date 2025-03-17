3921a48ce8fadb22e5666aad40c8cdc24e98a027
abhishek <abhishekmankame962@gmail.com>
2025-03-17 11:37:51 +0530

    space_complexity_update

--- old_data/stl/time_complexity/divide_array_into_equal_pairs.cpp
+++ old_data/stl/time_complexity/divide_array_into_equal_pairs.cpp
@@ -30,3 +30,9 @@ int main(){
 // 1. The code snippet is using the bitset to solve the problem.
 // 2. The code snippet is using the flip() and none() functions of the bitset, which takes constant 
 // 3. Here we are iterating over the nums array to flip the bits of the bitset which takes O(n) time --> Linear time
+// Hence, the time complexity of the code snippet is O(n) where n is the size of the nums array.
+
+// Let's analyze the space complexity of the code snippet:
+// 1. The code snippet is using the bitset to solve the problem.
+// 2. Here we are using the bitset of size 21 to store the bits of the nums array.
+// 3. Hence, the space complexity of the code snippet is O(1) --> Constant space complexity.
