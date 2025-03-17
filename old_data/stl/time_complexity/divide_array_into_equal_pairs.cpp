04bb9a8498fd54b27b6c32a469c4bc3dcdfac206
abhishek <abhishekmankame962@gmail.com>
2025-03-17 11:28:25 +0530

    divide_array_into_equal_pairs_info_update

---
+++ old_data/stl/time_complexity/divide_array_into_equal_pairs.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+#include<bitset>
+using namespace std;
+
+/* Divide array into equal pairs
+You are given an integer array nums consisting of 2*n integers.
+You need to divide the array into n pairs such that:
+--> Each element belongs to exactly one pair
+--> The element present in a pair are equal
+
+Return true if nums can be divided into n pairs, otherwuise return false.
+
+*/
