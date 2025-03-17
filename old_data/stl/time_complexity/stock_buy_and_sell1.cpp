76ab55ca3b668173ee5f143be0a6e4a1c7a0ded6
abhishek <abhishekmankame962@gmail.com>
2025-03-17 12:29:11 +0530

    stock buy and selling 1 information update

---
+++ old_data/stl/time_complexity/stock_buy_and_sell1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+/*Stock Buy and Sell 1
+This is leetcode problem: 121. Best time to buy and sell stock
+Here we are given an array prices where prices[i] is the price of a given stock on the ith day.
+We need to find the maximum profit that can be obtained by buying and selling the stock once.
+If we have no profit, we need to return 0.
+
+Constraints:
+1. 1<=prices.length<=10^5
+2. 0<=prices[i]<=10^4
+
+*/
