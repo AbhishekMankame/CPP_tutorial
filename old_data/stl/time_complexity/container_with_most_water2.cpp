d34021f8c26b34b3e5b160fc208f8f2601d276fc
abhishek <abhishekmankame962@gmail.com>
2025-03-18 12:27:34 +0530

    container with most water using two pointers approach information update

---
+++ old_data/stl/time_complexity/container_with_most_water2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+/*Container with most water using Two Pointers Approach
+Ab yaha pe hum brute force approach ki jagah two pointers approach ka use karenge.
+Two pointers approach ka use karne se hum O(N) time complexity me answer nikal sakte hai.
+Two pointers approach ka use karne se hume ek left pointer aur ek right pointer use karna hota hai.
+Yaha pe hum left pointer ko 0th index pe aur right pointer ko last index pe initialize karenge.
+
+*/
