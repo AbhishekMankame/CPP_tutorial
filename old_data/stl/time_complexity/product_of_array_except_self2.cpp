f4d2bf45cb9d79b319cf48b5842dc1b5433f7486
abhishek <abhishekmankame962@gmail.com>
2025-03-19 10:52:21 +0530

    product of array except self code

---
+++ old_data/stl/time_complexity/product_of_array_except_self2.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+/*Let us solve the product of array except self with optimal time complexity O(1) but here space complexity will be O(N) which is not optimized*/
+
+vector<int> productExceptSelf(vector<int> &nums){
+    int n = nums.size();
+    vector<int> prefix(n,1), suffix(n,1), result(n,1);
+    for(int i=1;i<n;i++){
+        prefix[i]=nums[i-1]*prefix[i-1];
+    }
+    for(int i=n-2;i>=0;i--){
+        suffix[i]=nums[i+1]*suffix[i+1];
+    }
+    for(int i=0;i<n;i++){
+        result[i]=prefix[i]*suffix[i];
+    }
+
+    return result;
+}
+
+int main(){
+    vector<int> nums = {1,2,3,4};
+    int n=nums.size();
+    vector<int> result = productExceptSelf(nums);
+    for(int i=0;i<n;i++){
+        cout<<result[i]<<" ";
+    }
+}
