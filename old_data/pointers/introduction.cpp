12940364914ead26da653dd5620e27087b90021f
abhishek <abhishekmankame962@gmail.com>
2025-03-19 12:26:51 +0530

    null pointer update

--- old_data/pointers/introduction.cpp
+++ old_data/pointers/introduction.cpp
@@ -15,5 +15,10 @@ int main(){
     cout<<ptr2<<endl;
     cout<<*ptr2<<endl<<**ptr2<<endl;
     cout<<&ptr2<<endl;
+
+    int* nullPtr = NULL; //This is a null pointer
+    cout<<nullPtr<<endl;
+    //cout<<*nullPtr<<endl; This will give an error as null pointer is pointing to nothing
+
     return 0;
 }
