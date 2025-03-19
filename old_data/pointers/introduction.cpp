c27ee111aa6257aefa98ec5c3dd5c4d71b61a92d
abhishek <abhishekmankame962@gmail.com>
2025-03-19 12:13:27 +0530

    pointer to pointer update

--- old_data/pointers/introduction.cpp
+++ old_data/pointers/introduction.cpp
@@ -6,9 +6,14 @@ using namespace std;
 int main(){
     int a=10;
     int* ptr=&a;
+    int** ptr2=&ptr;
     cout<<a<<endl;
     cout<<&a<<endl;
     cout<<ptr<<endl;
     cout<<*ptr<<endl;
     cout<<&ptr<<endl;
+    cout<<ptr2<<endl;
+    cout<<*ptr2<<endl<<**ptr2<<endl;
+    cout<<&ptr2<<endl;
+    return 0;
 }
