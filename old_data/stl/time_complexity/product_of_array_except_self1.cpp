4dfa71015b320e4beea9d89aaf5ac93b801c335b
abhishek <abhishekmankame962@gmail.com>
2025-03-19 09:31:35 +0530

    brute force code update

--- old_data/stl/time_complexity/product_of_array_except_self1.cpp
+++ old_data/stl/time_complexity/product_of_array_except_self1.cpp
@@ -9,5 +9,31 @@ This is leetcode question 238. Given an integer array nums, return an array answ
 Note: Here, we are not allowed to use division operation. 
 Constraints: 2 <=nums.lenth <= 10^5, -30 <= nums[i] <= 30
 
-
 */
+
+// Let us solve this using brute force approach
+
+vector<int> productExceptSelf(vector<int>& nums){
+    int n = nums.size();
+    vector<int> ans;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(i!=j){
+                ans[i] *=nums[j];
+            }
+        }
+    }
+
+    return ans;
+}
+
+int main() {
+    vector<int> nums = {1,2,3,4};
+    int n=nums.size();
+    vector<int> ans = productExceptSelf(nums);
+    for(int i=0;i<n;i++){
+        cout<<ans[i]<<" ";
+    }
+
+    return 0;
+}
