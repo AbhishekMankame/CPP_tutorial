e2492fcdb36fbe0c361822a10552e24390f24365
abhishek <abhishekmankame962@gmail.com>
2025-03-21 19:03:19 +0530

    binary search description update

--- old_data/stl/binary_search.cpp
+++ old_data/stl/binary_search.cpp
@@ -11,6 +11,14 @@ typedef long long int ll;
 --> The data structure must be sorted
 --> Access to any element of the data structure should take constant time
 
+*Binary Search Algorithm steps:
+--> Divide the search space into two halves by finding the middle index "mid"
+--> Compare the middle element of the search space with the key
+--> If the key is found at the middle element, the process is terminated
+--> If the key is not found at middle element, choose which half will be used as the next search space
+    --> If the key is smaller than the middle element, then the left side is used for next search
+    --> If the key is larger than the middle element, then the right side is used for next search
+
 */
 
 int main(){
