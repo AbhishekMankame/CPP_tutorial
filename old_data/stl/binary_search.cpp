632eed850d7cc6b42cb4284f6db7744fb826a9e4
abhishek <abhishekmankame962@gmail.com>
2025-03-21 18:55:36 +0530

    binary search information update

---
+++ old_data/stl/binary_search.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+typedef long long int ll;
+
+// Binary Search Algorithm
+// Binary search algorithm is a searching algoritm used in a sorted array by repeatedly dividing the search interval in half. The idea of bindary search is to use the information
+// that the array is sorted and reduce the time complexity to O(log N)
+
+/* Conditions to apply Binary Search Algorithm in a Data Structure
+--> The data structure must be sorted
+--> Access to any element of the data structure should take constant time
+
+*/
+
+int main(){
+    
+}
