ceca4dea468f6dfc2d759f9b795d130db38669ea
abhishek <abhishekmankame962@gmail.com>
2025-03-21 18:08:06 +0530

    simple_update

--- old_data/pointers/array_pointer.cpp
+++ old_data/pointers/array_pointer.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 // Array Pointer - Array name is a pointer to the first element of the array
+// Array pointer are constant pointers, they cannot be changed
 
 int main(){
     int arr[]={1,2,3,4,5};
