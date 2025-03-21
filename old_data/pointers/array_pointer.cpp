87aed2fb403bd6b7f04632d0021998cc89c9b926
abhishek <abhishekmankame962@gmail.com>
2025-03-21 18:05:09 +0530

    information update

--- old_data/pointers/array_pointer.cpp
+++ old_data/pointers/array_pointer.cpp
@@ -2,6 +2,8 @@
 #include<vector>
 using namespace std;
 
+// Array Pointer - Array name is a pointer to the first element of the array
+
 int main(){
     int arr[]={1,2,3,4,5};
     cout<<arr<<endl;
