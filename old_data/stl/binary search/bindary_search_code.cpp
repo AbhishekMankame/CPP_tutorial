fc97a065142513afaaa5c69e14feacab7375d48d
abhishek <abhishekmankame962@gmail.com>
2025-03-23 00:20:33 +0530

    binary search code update

---
+++ old_data/stl/binary search/bindary_search_code.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int binarySearch(vector<int> arr, int tar){
+    int st=0;
+    int end=arr.size()-1;
+
+    while(st<=end){
+        int mid=(st+end)/2;
+        if(tar>arr[mid]){
+            st=mid+1;
+        }
+        else if(tar<arr[mid]){
+            end=mid-1;
+        }
+        else{
+            return mid;
+        }
+    }
+    return -1;
+
+}
+
+int main(){
+    vector<int> arr = {-1,8,3,4,5,9,12}; //with odd number of elements
+    int tar1=12;
+    cout<<binarySearch(arr,tar1)<<endl;
+
+    return 0;
+}
