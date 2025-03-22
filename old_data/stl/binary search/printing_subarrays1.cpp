370f36d908a469676fb81173f948653496c3b93a
abhishek <abhishekmankame962@gmail.com>
2025-03-22 15:19:26 +0530

    correct code update

--- old_data/stl/binary search/printing_subarrays1.cpp
+++ old_data/stl/binary search/printing_subarrays1.cpp
@@ -5,7 +5,7 @@ using namespace std;
 void printAllSubarrays(int arr[], int n){
     for(int i=0;i<n;i++){
         for(int j=i;j<n;j++){
-            for(int k=i+1;k<j;k++){
+            for(int k=i;k<j;k++){
                 cout<<arr[k]<<" ";
             }
             cout<<" ";
