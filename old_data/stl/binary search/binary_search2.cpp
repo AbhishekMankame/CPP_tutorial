a449fcc5e7b16e4adaf57e003b350cfc5fdc243f
abhishek <abhishekmankame962@gmail.com>
2025-03-23 12:53:46 +0530

    binary search using recursion update

---
+++ old_data/stl/binary search/binary_search2.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int binarySearch(vector<int>& arr, int target, int st, int end){
+    if(st<=end){
+        int mid=st+(end-st)/2;
+        if(target>arr[mid]){
+            return binarySearch(arr,target,mid+1,end);
+        }
+        else if(target<arr[mid]){
+            return binarySearch(arr,target,st,mid-1);
+        }
+        else{
+            return mid;
+        }
+    }
+    return -1;
+}
+
+int main(){
+    vector<int> arr = {-3,-1,0,4,6,7,8,9};
+    int n=sizeof(arr)/sizeof(int);
+    int st=0, end=arr.size()-1;
+    int tar=5;
+    cout<<binarySearch(arr,tar,st,end);
+
+    return 0;
+}
