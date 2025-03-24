e57c4ba843551b23dcdc095de2bda8f6e2e36d8f
abhishek <abhishekmankame962@gmail.com>
2025-03-24 23:16:52 +0530

    stable and hybrid sort update

--- old_data/stl/sorting/bubble_sort.cpp
+++ old_data/stl/sorting/bubble_sort.cpp
@@ -12,7 +12,8 @@ Sorting Basics--
 -> In-place sorting: An in-place sorting algorithm uses constant space for producing the output (modifies the given array only). Examples: Selection sort, Bubble sort, Insertion sort, Heap sort
 -> Internal sorting: Internal sorting is when all the data is placed in the main memory or internal memory. In internal sorting, the problem cannot take input beyond allocated memnory size.
 -> External sorting: External sorting is when all the data that needs to be sorted need not to be placed in memory at a time, the sorting is called external sorting. External sorting is used for the massive amount of data. For examples Merge sort can be used in external sorting as the whole array does not have to be present all the time in memory. 
--->
+-> Stable sorting: When two same items appear in the same order in sorted data as in the original array called stable sort. Examples: Merge sort, Insertion sort, Bubble sort. 
+-> Hybrid sorting: A sorting algorithm is called Hybrid if it uses more than one standard sorting algorithms to sort the array. The idea is to take advantages of multiple sorting algorithms. For example: IntroSort uses insertion sort and quick sort.
 
 
 
