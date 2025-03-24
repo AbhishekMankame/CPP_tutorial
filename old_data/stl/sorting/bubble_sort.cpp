e98a7bd6dcffae53a19a5bd01a1533fd298a4c56
abhishek <abhishekmankame962@gmail.com>
2025-03-24 23:11:13 +0530

    internal sorting details update

--- old_data/stl/sorting/bubble_sort.cpp
+++ old_data/stl/sorting/bubble_sort.cpp
@@ -6,6 +6,12 @@
 --> Sorting refers to rearrangement of a given array or list of elements according to a comparison operator on the elements. The comparison operator is used to decide the new order of elements in the respective data structure.
 --> Sorting Algorithm is used to rearrange a given array or list of elements in an order. Sorting is provided in library implementation of most of the programming languages.
 
+The sorting algorithm is important in Computer Science because it reduces the complexity of a problem. There is a wide range of applications for these algorithms, including searching algorithms, database algorithms, divide and conquer methods, and data structure algorithms. 
+
+Sorting Basics--
+-> In-place sorting: An in-place sorting algorithm uses constant space for producing the output (modifies the given array only). Examples: Selection sort, Bubble sort, Insertion sort, Heap sort
+-> Internal sorting: Internal sorting is when all the data is placed in the main memory or internal memory. In internal sorting, the problem cannot take input beyond allocated memnory size.
+
 
 
 */
