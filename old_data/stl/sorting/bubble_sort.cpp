928e5c496b2155c61cb7e754aa199ea738a4f0d9
abhishek <abhishekmankame962@gmail.com>
2025-03-25 11:11:57 +0530

    bubble sort details update

--- old_data/stl/sorting/bubble_sort.cpp
+++ old_data/stl/sorting/bubble_sort.cpp
@@ -1,24 +1,17 @@
 #include<iostream>
 #include<vector>
-#include<algorithm>
+using namespace std;
 
-/* Sorting
---> Sorting refers to rearrangement of a given array or list of elements according to a comparison operator on the elements. The comparison operator is used to decide the new order of elements in the respective data structure.
---> Sorting Algorithm is used to rearrange a given array or list of elements in an order. Sorting is provided in library implementation of most of the programming languages.
+// Bubble sort:
+// --> Bubble sort is a simple sorting algorithm that works by repeatedly stepping through the list, comparing each pair of adjacent items and swapping them if they are in the wrong order.
+// --> The pass through the list is repeated until no swaps are needed, which indicates that the list is sorted.
+// --> The algorithm gets its name from the way smaller elements "bubble" to the top of the list.
+// --> Bubble sort is not a practical sorting algorithm when n is large.
+// --> Bubble sort has a worst-case and average complexity of O(N^2) when n is large.
 
-The sorting algorithm is important in Computer Science because it reduces the complexity of a problem. There is a wide range of applications for these algorithms, including searching algorithms, database algorithms, divide and conquer methods, and data structure algorithms. 
-
-Sorting Basics--
--> In-place sorting: An in-place sorting algorithm uses constant space for producing the output (modifies the given array only). Examples: Selection sort, Bubble sort, Insertion sort, Heap sort
--> Internal sorting: Internal sorting is when all the data is placed in the main memory or internal memory. In internal sorting, the problem cannot take input beyond allocated memnory size.
--> External sorting: External sorting is when all the data that needs to be sorted need not to be placed in memory at a time, the sorting is called external sorting. External sorting is used for the massive amount of data. For examples Merge sort can be used in external sorting as the whole array does not have to be present all the time in memory. 
--> Stable sorting: When two same items appear in the same order in sorted data as in the original array called stable sort. Examples: Merge sort, Insertion sort, Bubble sort. 
--> Hybrid sorting: A sorting algorithm is called Hybrid if it uses more than one standard sorting algorithms to sort the array. The idea is to take advantages of multiple sorting algorithms. For example: IntroSort uses insertion sort and quick sort.
-
-Type of sorting techniques--
-There are various sorting algorithms are used in data structures. The following two types of sorting algorithms can be broadly classifies:
-1. Comparison-based sorting: These algorithms use comparison operators to compare elements for sorting the elements. Examples: Bubble sort, Selection sort, Insertion sort, Merge sort, Quick sort, Heap sort. 
-2. Non-comparison-based sorting: These algorithms do not use comparison operators to compare elements for sorting the elements. Examples: Counting sort, Radix sort, Bucket sort.
+int main(){
 
 
-*/
+
+    return 0;
+}
---
+++ old_data/stl/sorting/sorting_basics.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+/* Sorting
+--> Sorting refers to rearrangement of a given array or list of elements according to a comparison operator on the elements. The comparison operator is used to decide the new order of elements in the respective data structure.
+--> Sorting Algorithm is used to rearrange a given array or list of elements in an order. Sorting is provided in library implementation of most of the programming languages.
+
+The sorting algorithm is important in Computer Science because it reduces the complexity of a problem. There is a wide range of applications for these algorithms, including searching algorithms, database algorithms, divide and conquer methods, and data structure algorithms. 
+
+Sorting Basics--
+-> In-place sorting: An in-place sorting algorithm uses constant space for producing the output (modifies the given array only). Examples: Selection sort, Bubble sort, Insertion sort, Heap sort
+-> Internal sorting: Internal sorting is when all the data is placed in the main memory or internal memory. In internal sorting, the problem cannot take input beyond allocated memnory size.
+-> External sorting: External sorting is when all the data that needs to be sorted need not to be placed in memory at a time, the sorting is called external sorting. External sorting is used for the massive amount of data. For examples Merge sort can be used in external sorting as the whole array does not have to be present all the time in memory. 
+-> Stable sorting: When two same items appear in the same order in sorted data as in the original array called stable sort. Examples: Merge sort, Insertion sort, Bubble sort. 
+-> Hybrid sorting: A sorting algorithm is called Hybrid if it uses more than one standard sorting algorithms to sort the array. The idea is to take advantages of multiple sorting algorithms. For example: IntroSort uses insertion sort and quick sort.
+
+Type of sorting techniques--
+There are various sorting algorithms are used in data structures. The following two types of sorting algorithms can be broadly classifies:
+1. Comparison-based sorting: These algorithms use comparison operators to compare elements for sorting the elements. Examples: Bubble sort, Selection sort, Insertion sort, Merge sort, Quick sort, Heap sort. 
+2. Non-comparison-based sorting: These algorithms do not use comparison operators to compare elements for sorting the elements. Examples: Counting sort, Radix sort, Bucket sort.
+
+
+*/
