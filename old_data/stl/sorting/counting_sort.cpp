d736173e76729f91e93ef40063718cbd33ad79f7
abhishek <abhishekmankame962@gmail.com>
2025-03-26 00:48:37 +0530

    couting sort code update

--- old_data/stl/sorting/counting_sort.cpp
+++ old_data/stl/sorting/counting_sort.cpp
@@ -22,3 +22,25 @@ Input data: 1, 4, 1, 2, 7, 5, 2
     The modified count array indicates the position of each object in the output sequence. 
 
 */
+
+void counting_sort(int arr[], int n){
+    // Largest Element
+    int largest = -1;
+    for(int i=0;i<n;i++){
+        largest=max(largest,arr[i]);
+    }
+
+    // Create a count array/vector
+    //int *freq=new int[largest+1] --> This is dynamic array creation
+    vector<int> freq(largest+1,0);
+}
+
+int main(){
+    int arr[] = {88,97,10,12,15,1,5,6,12,5,8};
+    int n=sizeof(arr)/sizeof(int);
+    counting_sort(arr,n);
+
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
