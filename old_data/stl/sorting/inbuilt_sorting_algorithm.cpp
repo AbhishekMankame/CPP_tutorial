31a9ec3c1c35f4a40b1b1c0b2588a1906de6cb59
abhishek <abhishekmankame962@gmail.com>
2025-03-25 23:21:32 +0530

    inbuilt sort complexity update

--- old_data/stl/sorting/inbuilt_sorting_algorithm.cpp
+++ old_data/stl/sorting/inbuilt_sorting_algorithm.cpp
@@ -14,3 +14,17 @@ int main(){
 
     return 0;
 }
+
+/* Let's talk about Inbuilt sorting algorithm in C++
+Inbuilt sorting algorithm in C++ is a hybrid sorting algorithm. It is a combination of Quick Sort, Heap Sort and Insertion Sort.
+It is the most efficient sorting algorithm in C++. 
+It is implemented in the header file <algorithm> and the function is sort(). 
+Syntax: sort(starting address, ending address)
+It takes two arguements, the starting address of the array and the ending address of the array. 
+It sorts the array in increasing order by default. 
+It is a stable sorting algorithm. 
+
+Time Complexity: O(NlogN) in the average and worst case. 
+Space Complexity: O(1) as no extra space is used. 
+
+*/
