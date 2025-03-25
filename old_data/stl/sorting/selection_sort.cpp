dbb74f941a57c2711ba874477b67a05b6769b424
abhishek <abhishekmankame962@gmail.com>
2025-03-25 22:56:10 +0530

    selection sort information update

---
+++ old_data/stl/sorting/selection_sort.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+/* Selection Sort
+Selection sort is a simple sorting algorithm. It selects the smallest element from the unsorted portion of the array and swaps it with the first element of the array. 
+It then selects the second smallest element from the unsorted portion and swaps it with the second element of the array, and same goes on. 
+It is not suitable for large data sets as its average and worst case complexities are of O(N^2), where N is the number of items.
+
+*/
