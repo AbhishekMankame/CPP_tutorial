2f11346c2a2b50df86255651509ca3d006abe01c
abhishek <abhishekmankame962@gmail.com>
2025-03-26 10:19:35 +0530

    character array introduction update

---
+++ old_data/stl/character_array/introduction.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+/* Character array
+ Character arrray is a data structure that stores a sequence of characters. 
+ It is a null terminated array of characters. 
+ 
+ Creating a Char array
+ char a[100];
+ 
+ char a[100]={'a','b','c','d','e'}; // Wrong way
+
+ char a[100]={'a','b','c','d','e','\0'}; // Correct way
+
+ char a[100]="hello"; // Automatically appends '\0' at the end
+*/
