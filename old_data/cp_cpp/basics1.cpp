ea98b96bc8b8727926379dab496bf2f01b8212f5
abhishek <abhishekmankame962@gmail.com>
2025-03-28 23:58:01 +0530

    siimple_update

--- old_data/cp_cpp/basics1.cpp
+++ old_data/cp_cpp/basics1.cpp
@@ -10,7 +10,6 @@ int main(){
     // Calculations joh bhi hoti hai, woh higher data type me hoti hai
     cout<<'c'+1<<endl; // This will give 100, as the ASCII value of 'c' is 99
     cout<<'c'+'d'<<endl; // This will give 199, as the ASCII value of 'c' is 99 and 'd' is 100
-    
 
     return 0;
 }
