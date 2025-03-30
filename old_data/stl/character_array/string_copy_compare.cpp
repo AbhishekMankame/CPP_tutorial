dd7cee8c19798a10fc600106d870bb9540da1bf1
abhishek <abhishekmankame962@gmail.com>
2025-03-30 23:24:17 +0530

    strlen_update

---
+++ old_data/stl/character_array/string_copy_compare.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+#include<cstring>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+int main(){
+    char a[1000]="apple";
+    char b[1000];
+    cout<<strlen(a)<<endl; 
+}
