ab36863c90774b02011df39e1ec2336e5c973681
abhishek <abhishekmankame962@gmail.com>
2025-03-30 06:23:46 +0530

    C++ headerfiles details update

---
+++ old_data/stl/character_array/binary_string_to_number.cpp
@@ -0,0 +1,8 @@
+#include<iostream> // This is used for the cout and cin functions
+#include<algorithm> // This is used for the sort function
+#include<cstring> // This is used for the strlen function
+#include<cmath> // This is used for the pow function
+#include<limits.h> // This is used for the INT_MAX and INT_MIN constants
+#include<bitset> // This is used for the bitset class
+#include<string> // This is used for the string class
+using namespace std; // This is used to avoid writing std:: before every function
