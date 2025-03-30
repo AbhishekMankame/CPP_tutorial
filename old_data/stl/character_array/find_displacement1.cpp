6f9913c8886550c6fdf23ec39934e9eff23637c7
abhishek <abhishekmankame962@gmail.com>
2025-03-30 21:08:29 +0530

    find displacement logic update using switch

--- old_data/stl/character_array/find_displacement1.cpp
+++ old_data/stl/character_array/find_displacement1.cpp
@@ -8,5 +8,24 @@ using namespace std;
 Find Displacement: Given a long route containing N,S,E,W directions, find the shortest path to reach the location. 
 Sample Input: SNNNEWE
 Sample Output: NNE
+Note: Here we are finding the shortest path or displacement 
 
 */
+
+int main(){
+    char route[1000];
+    cin.getline(route,1000); // Read a line of text from the input string and store it in the character array 'route'
+    int x=0;
+    int y=0;
+    for(int i=0;route[i]!='\0';i++){
+        switch(route[i]){
+            case 'N': y++; break; // Move north (y coordinate increases)
+            case 'S': y--; break; // Move south (y coordinate decreases)
+            case 'E': x++; break; // Move east (x coordinate increases)
+            case 'W': x--; break; // Move west (x coordinate decreases)
+            default: break; // Ignore any other characters
+        }
+    }
+
+    return 0;
+}
