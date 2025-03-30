832d2efab452686bb237791a920ff064af274524
abhishek <abhishekmankame962@gmail.com>
2025-03-30 19:56:22 +0530

    cin.getline() details update

---
+++ old_data/stl/character_array/cin_geline.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<cstring>
+using namespace std;
+
+// cin.getline() --> cin.getline() is used to read a line of text from the input stream. It reads the characters until a newline character is encountered or the specified maximum number of characters is read.
+// It also discards the newline character from the input stream. The function takes two arguements: the character array to store the input and the maximum number of characters to read.
