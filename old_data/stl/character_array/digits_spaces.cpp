5ebd97f93acf7c2e17426d5d2ed8b0670e917142
abhishek <abhishekmankame962@gmail.com>
2025-03-30 19:44:17 +0530

    digits and space code update

---
+++ old_data/stl/character_array/digits_spaces.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<cstring>
+using namespace std;
+
+// Given a sentence, count the alphabets, number of digits and spaces in the sentence
+// and print the number of digits and spaces in the sentence.
+
+int main(){
+    // store and then count
+    // read one by one and then count
+
+    char ch;
+    ch=cin.get();
+
+    //count
+    int alpha=0;
+    int space=0;
+    int digit=0;
+
+    while(ch!='\n'){
+        if(ch>='0' and ch<='9'){
+            digit++;
+        }
+        else if(ch>='a' and ch<='z'){
+            alpha++;
+        }
+        else if(ch>='A' and ch<='Z'){
+            alpha++;
+        }
+        else if(ch==' ' or ch=='\t'){
+            space++;
+        }
+    }
+    cout<<"Total Alphabets: "<<alpha<<endl;
+    cout<<"Total Digits: "<<digit<<endl;
+    cout<<"Total Spaces: "<<space<<endl;
+    cout<<"Total Characters: "<<alpha+digit+space<<endl;
+
+}
