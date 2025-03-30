781d170ef64b3611a85fb3866715d11cfbe2d910
abhishek <abhishekmankame962@gmail.com>
2025-03-30 06:31:45 +0530

    binary string to integer code update

--- old_data/stl/character_array/binary_string_to_number.cpp
+++ old_data/stl/character_array/binary_string_to_number.cpp
@@ -6,3 +6,25 @@
 #include<bitset> // This is used for the bitset class
 #include<string> // This is used for the string class
 using namespace std; // This is used to avoid writing std:: before every function
+
+int binaryToDecimal(string s){
+    string temp=s; // Copying the string s to temp
+    int base=1;
+    int dec=0; 
+    int n=s.size(); // Getting the size of the string s
+    for(int i=n-1;i>=0;i--){ // looping through the string from the last character to the first character
+        if(temp[i]=='1'){ // If the character is 1, then we need to add the value of the bit to the decimal number
+            dec+=base; // Adding the value of the bit to the decimal number          
+        }
+        base*=2; // Multiplying the base by 2 for the next bit
+    }
+    return dec; // Returning the decimal number
+}
+
+int main(){
+    string s; // Declaring a string variable s
+    cout<<"Enter the binary number: "; // Prompting the user to enter a binary number
+    cin>>s; // Taking the input from the user
+    cout<<"The decimal number is: "<<binaryToDecimal(s)<<endl; // Calling the binaryToDecimal function and printing the result
+    cout<<"The decimal number is: "<<stoi(s,0,2)<<" using STOI function"<<endl; 
+}
