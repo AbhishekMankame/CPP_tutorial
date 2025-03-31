e00b55c2a30d6da9039b6c2f88bdb8a3c378701b
abhishek <abhishekmankame962@gmail.com>
2025-03-31 13:49:42 +0530

    string compression details update

---
+++ old_data/stl/character_array/string_comression1.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<cstring>
+using namespace std;
+
+/* String Compression
+Given an array of characters chars, compress it using the following algorithm:
+Begin with an empty string s. For each group of consecutive repeating characters in chars: 
+- If the group's length is 1, append the character to s. 
+- Otherwise, append the character followed by the group's length. 
+
+The compressed string s should not be returned separately, but instead be stored in the input character array chars. Note that group lengths that are 10 or longer will be split into mulltiple characters in chars. 
+After you are done modifying the input array, return the new length of the array. 
+
+Input Format: In the function a vector of characters is passed. 
+Output Format: Return the updated vector
+
+*/
+
+int main()[
+
+]
