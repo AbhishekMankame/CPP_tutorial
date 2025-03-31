96328cecfd19485c0261e2c78d189e0c532c431a
abhishek <abhishekmankame962@gmail.com>
2025-03-31 14:08:55 +0530

    string compression code and output

--- old_data/stl/character_array/string_comression1.cpp
+++ old_data/stl/character_array/string_comression1.cpp
@@ -17,8 +17,42 @@ After you are done modifying the input array, return the new length of the array
 Input Format: In the function a vector of characters is passed. 
 Output Format: Return the updated vector
 
+Note: If compressed string is bigger than original return the original string. 
+Input: aaabbccddee
+Output: a3b2c2d2e2
+
+Input: abcd
+Output: abcd
+
 */
 
-int main()[
+string compressString(string str){
+    int n=str.length();
+    string output;
+    for(int i=0;i<n;i++){
+        int count=1;
+        while(i<n-1 and str[i+1]==str[i]){ 
+            count++;
+            i++;
+        }
+        output+=str[i]; // add the character to output
+        output+=to_string(count); // add the count to output
+    }
+    if(output.length()>str.length()){
+        return str; // return original string if compressed string is bigger than original
+    }
+    else{
+        return output; // return compressed string
+    }
 
-]
+}
+
+int main(){
+    string s1="aaabbccddee"; // This string uses dynamic array internally, so size can be changed in runtime also
+    cout<<compressString(s1)<<endl;
+
+    string s2="abcd";
+    cout<<compressString(s2)<<endl;
+
+    return 0;
+}
