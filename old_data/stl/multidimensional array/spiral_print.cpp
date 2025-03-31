c28d9311cae8bece45da146f0cbc03244afaef92
abhishek <abhishekmankame962@gmail.com>
2025-03-31 20:29:08 +0530

    spiral print details update

---
+++ old_data/stl/multidimensional array/spiral_print.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<cstring>
+#include<string>
+using namespace std;
+
+/*Spiral Print
+Write a function that takes in nxm 2D array, and prints the all array elements in a spiral order. Start from the top left corner goes to right, proceeds in a spiral pattern until every element is visited. 
+Input: 1 2 3 4
+       5 6 7 8
+       9 10 11 12
+Output: 1 2 3 4 5 12 11 10 9 8 7 6
+
+
+*/
