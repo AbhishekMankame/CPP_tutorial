a0219e1b4ba7b12c22c938814ad1d9283d8a7e4a
abhishek <abhishekmankame962@gmail.com>
2025-03-31 10:44:27 +0530

    largest string code update

--- old_data/stl/character_array/largest_string1.cpp
+++ old_data/stl/character_array/largest_string1.cpp
@@ -8,5 +8,22 @@ using namespace std;
 // Problem: Read N strings and print the largest string. Each string can have upto 1000 characters.
 
 int main(){
-
+    int n;
+    cin>>n;
+    cin.ignore(); // Ignore the newline character after reading n
+    // Note here we can also use cin.get() to consume that newline character.
+    char sentence[1000]; // To store the largest string
+    char largest[1000];
+    int largest_len=0; // To store the length of the largest string
+    while(n--){
+        cin.getline(sentence, 1000); // Read a string with spaces
+        //cout<<sentence<<endl;
+        int len = strlen(sentence);
+        if(len>largest_len){
+            largest_len=len; // Update the length of the largest string
+            strcpy(largest, sentence); // Copy the largest string
+        }
+    }
+    cout<<"Largest sentence is: "<<largest<<endl; // Print the largest string
+    return 0;
 }
