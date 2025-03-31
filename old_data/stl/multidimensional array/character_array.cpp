1cbcbfec3f0ad45ddafc55eba25cb4fbc66a9718
abhishek <abhishekmankame962@gmail.com>
2025-03-31 20:19:20 +0530

    character array details update

---
+++ old_data/stl/multidimensional array/character_array.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<cstring>
+#include<string>
+using namespace std;
+
+/* Multidimensional character array
+Multidimensional character array is a 2D array of characters. 
+It is used to store strings in a 2D format. 
+
+*/
+
+int main(){
+    char numbers[][10]={"one","two","three","four"};
+
+    for(int i=0;i<numbers[i][0];i++){
+        cout<<numbers[i]<<" "; // Print each string in the array
+    }
+}
