d946ff4f651dfdbc63f87084d68f98423c188405
abhishek <abhishekmankame962@gmail.com>
2025-04-02 17:06:57 +0530

    spiral print code update

--- old_data/stl/multidimensional array/spiral_print.cpp
+++ old_data/stl/multidimensional array/spiral_print.cpp
@@ -15,13 +15,55 @@ Output: 1 2 3 4 5 12 16 15 14 13 9 5 6 7 11 10
 
 */
 
+void print(int arr[][10], int n, int m){
+    // 4 variables to track the boundaries of the array
+    int startRow=0;
+    int endRow=n-1;
+    int startCol=0;
+    int endCol=m-1;
+
+    // Outer loop (Traverse array boundary)
+
+    while(startCol<=endCol){
+
+        // Start Row
+        for(int col=startCol;col<=endCol;col++){
+            cout<<arr[startRow][col];
+        }
+
+        // End Column
+        for(int row=startRow+1;row<=endRow;row++){
+            cout<<arr[row][endCol];
+        }
+
+        // End Row
+        for(int col=endCol-1;col>=startCol;col--){
+            cout<<arr[endRow][col];
+        }
+
+        // Start Column
+        for(int row=endRow-1;row>=startRow+1;row--){
+            if(startCol==endCol){
+                break; // Avoid printing the same element again if it's a single column
+            }
+            cout<<arr[row][startCol];
+        }
+
+        // Update the variables to point to inner spiral
+        startRow++;
+        endRow--;
+        startCol++;
+        endCol--;
+    }
+}
+
 int main(){
     //Print in spiral form
     int arr[][4]={{1,2,3,4},
                   {5,6,7,8},
                   {9,10,11,12},
                   {13,14,15,16}};
-
+    int n=4,m=4;
 
     return 0;
 }
