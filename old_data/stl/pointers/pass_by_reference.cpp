d668d8319c269b2a3687ee288edf2f31bf0d3d94
abhishek <abhishekmankame962@gmail.com>
2025-04-03 11:13:45 +0530

    simple_update

--- old_data/stl/pointers/pass_by_reference.cpp
+++ old_data/stl/pointers/pass_by_reference.cpp
@@ -5,14 +5,14 @@
 #include<algorithm>
 using namespace std;
 
-// Pass by reference
+// Pass by reference using reference variable
 
 void applyTax(int &income){
     float tax=0.10;
     income = (1-tax)*income; // or can also be written as income = income - income*tax
     // Note: here when we passed int income in the function, at that time the output in main did not get updated during the output.
     // We gave value of n as 100, instead of printing the output as 90, it printed 100, but when we passed by reference, we have got the correct value as output.
-    
+    cout<<income<<endl;
 }
 
 int main(){
