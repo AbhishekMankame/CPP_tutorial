05bd1335b2c31749e29e5ceb2d6ca834e06eb766
abhishek <abhishekmankame962@gmail.com>
2025-04-04 19:48:22 +0530

    vector introduction code update

--- stl/vector/introduction.cpp
+++ stl/vector/introduction.cpp
@@ -7,4 +7,15 @@ int main()
     vector<int> v;
     int n;
     cout<<"Enter the size of the vector: ";
+    cin>>n;
+    cout<<"Enter the elements of the vector: ";
+    for(int i=0;i<n;i++){
+        int x;
+        cin>>x;
+        v.push_back(x);
+    }
+    cout<<"The elements of the vector are: ";
+    for(int i=0;i<n;i++){
+        cout<<v[i]<<" ";
+    }
 }
