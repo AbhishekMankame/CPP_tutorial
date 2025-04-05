6024c7829d63a7c33061a0517480faaaca46ee23
abhishek <abhishekmankame962@gmail.com>
2025-04-05 12:31:53 +0530

    vector details update

--- stl/vector/introduction.cpp
+++ stl/vector/introduction.cpp
@@ -2,6 +2,15 @@
 #include<vector>
 using namespace std;
 
+/* Some information about vectors
+--> Vectors are dynamic arrays that can grow and shrink in size.
+--> They are part of the C++ Standard Template Library (STL).
+--> Vectors are sequence containers representing arrays that can change in size. 
+--> Just like arrays, vectors use contiguous storage locations for their elements, which means that their elemetns can also be accessed directly in O(1) time. (Constant time)
+--> But unlike arrays, their size can change dynamically, with their storage being handled automatically by the container. 
+
+*/
+
 int main()
 {
     vector<int> v;
