08f63419780838f107314e0a3f9e9db6152ec50c
abhishek <abhishekmankame962@gmail.com>
2025-04-06 11:15:05 +0530

    details_update

--- stl/vector/vector_basics.cpp
+++ stl/vector/vector_basics.cpp
@@ -7,4 +7,16 @@ int main(){
     cout<<"Size of vector: "<<arr.size()<<endl; // Size of vector is 0
     cout<<"Capacity of vector: "<<arr.capacity()<<endl; // Capacity of vector is 0
     cout<<"Max size of vector: "<<arr.max_size()<<endl; // Max size of vector is 2^32-1
+
+    arr.push_back(1); // Adding 1 to the vector
+    cout<<"Size of vector: "<<arr.size()<<endl; // Size of vector is 1
+    cout<<"Capacity of vector: "<<arr.capacity()<<endl; // Capacity of vector is 1
+
+    arr.push_back(2); // Adding 2 to the vector
+    cout<<"Size of vector: "<<arr.size()<<endl;
+    cout<<"Capacity of vector: "<<arr.capacity()<<endl;
+
+    arr.push_back(3); // Adding 3 to the vector
+    cout<<"Size of vector: "<<arr.size()<<endl;
+    cout<<"Capacity of vector: "<<arr.capacity()<<endl;
 }
