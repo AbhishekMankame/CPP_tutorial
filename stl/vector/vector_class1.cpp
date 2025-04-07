ffb5f7830bfce9260cfbe028195ee41526867eb1
abhishek <abhishekmankame962@gmail.com>
2025-04-07 16:46:16 +0530

    output_update

--- stl/vector/vector_class1.cpp
+++ stl/vector/vector_class1.cpp
@@ -72,6 +72,18 @@ public:
 int main(){
     Vector v;
     v.push_back(1);
+    cout<<v.size();
+    cout<<endl<<v.capacity()<<endl;
+    v.push_back(2);
+    cout<<v.size()<<endl;
+    cout<<v.capacity()<<endl;
+    v.push_back(3);
+    v.push_back(4);
+    cout<<v.size()<<endl;
+    cout<<v.capacity()<<endl;
+    v.push_back(5);
+    cout<<v.size()<<endl;
+    cout<<v.capacity()<<endl;
 
     return 0;
 
