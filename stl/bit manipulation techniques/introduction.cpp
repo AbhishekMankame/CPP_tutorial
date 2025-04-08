c9d332a1b6c4e2f15ca17bb3afd8c32ba0bf3d2c
abhishek <abhishekmankame962@gmail.com>
2025-04-08 09:44:16 +0530

    bitwise operators introduction

---
+++ stl/bit manipulation techniques/introduction.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+
+/*Bit Manipulation techniques
+
+--> Reason why we use bitwise operator is that they operate on the binary representation of the data. 
+--> It is fast as compared to non bitwise operators
+
+==> Different types of bitwise operators are:
+--> Binary AND &
+--> Binary OR |
+--> Binary XOR ^
+--> Binary One's Complement ~
+--> Binary Left Shift <<
+--> Binary Right Shift >> 
+
+
+
+*/
+
+int main(){
+
+}
