d7d07549c6d6906c0a3d6bf1ee27228aafe5e303
abhishek <abhishekmankame962@gmail.com>
2025-04-09 09:50:12 +0530

    even or odd number using bitwise operator

--- stl/bit manipulation techniques/intro2.cpp
+++ stl/bit manipulation techniques/intro2.cpp
@@ -6,4 +6,23 @@ using namespace std;
 int main(){
     cout<<(5<<2)<<endl; // left shift
     cout<< (5>>2) <<endl; // right shift
+    // Note: Left shift does multiplication and right shift divides
+    // a<<b = a*(2^b)
+    // a>>b = a/(2^b)
+
+    /*Odd or Even
+    Write a function or expression to check if a number is odd or even!! using bitwise operator
+    Note: all the odd numbers will have last bit as 1 and all the even numbers will have last bit as 0
+    Here last bit decides whether the given number is odd or even.
+    */
+
+    int x;
+    cin>>x;
+    if(x&1){
+        cout<<"Odd"<<endl;
+    }
+    else{
+        cout<<"Even"<<endl;
+    }
+
 }
