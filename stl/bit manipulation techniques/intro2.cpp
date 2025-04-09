967b4b59106b61a5a9100fd89e408be395e5efe7
abhishek <abhishekmankame962@gmail.com>
2025-04-09 09:34:01 +0530

    left shift and right shift

--- stl/bit manipulation techniques/intro2.cpp
+++ stl/bit manipulation techniques/intro2.cpp
@@ -1,6 +1,9 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// Let's see about binary left shift and binary right shift operator
+
 int main(){
-    cout<<"Hello"<<endl;
+    cout<<(5<<2)<<endl; // left shift
+    cout<< (5>>2) <<endl; // right shift
 }
