84696fce1596c489e0f8469080c590b8251070b9
abhishek <abhishekmankame962@gmail.com>
2025-04-09 23:34:34 +0530

    int overflow details update

--- cp_cpp/basics1.cpp
+++ cp_cpp/basics1.cpp
@@ -11,6 +11,14 @@ int main(){
     cout<<'c'+1<<endl; // This will give 100, as the ASCII value of 'c' is 99
     cout<<'c'+'d'<<endl; // This will give 199, as the ASCII value of 'c' is 99 and 'd' is 
     
+    // Note: int ki rough range -10^9 to 10^9
+    // long int ki rough range -10^12 to 10^12
+    // long long int ki rough range -10^18 to 10^18    
+
+    int a=100000;
+    int b=100000;
+    int c = a*b; // yaha par overflow ho jaayega
+
 
     return 0;
 }
