4624ff53e6548817217c69da4a0c79bcdebdaae7
abhishek <abhishekmankame962@gmail.com>
2025-04-10 09:39:09 +0530

    simple_update

--- old_data/stl/character_array/strings.cpp
+++ old_data/stl/character_array/strings.cpp
@@ -3,8 +3,13 @@ using namespace std;
 
 // Strings are sequence of characters that are ussed to store words and text.They are also used to store data, such as numbers and other types of information in the form of text.
 // Strings are provided by <string> header file in the form of std::string class.
+// Note: strings joh hai woh double quote mei aati hai, and character single quote mei aati hai
 
 int main(){
     string str="Hello";
     cout<<str<<endl;
+    string str2;
+    cin>>str2;
+    cout<<str2<<endl;
+    cout<<str+str2<<endl;
 }
