d798bc5ebdd32c1388748641e125999751fb3ae3
abhishek <abhishekmankame962@gmail.com>
2025-04-11 16:10:46 +0530

    code with testcase update

--- old_data/stl/character_array/getline1.cpp
+++ old_data/stl/character_array/getline1.cpp
@@ -10,6 +10,15 @@ int main(){
     getline(cin,str2);
     cout<<str1<<endl<<str2<<endl;
 
+    int t;
+    cin>>t;
+    cin.ignore(); // Here we use cin.ignore() to ignore the cursor space
+    while(t--){
+        string s;
+        getline(cin,s);
+        cout<<s<<endl;
+    }
+
     return 0;
 }
 
