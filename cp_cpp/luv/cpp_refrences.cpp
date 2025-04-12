97bec053bf96d26675778e7ba69a924b66133b53
abhishek <abhishekmankame962@gmail.com>
2025-04-12 12:48:26 +0530

    c++ refrences update

---
+++ cp_cpp/luv/cpp_refrences.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void increment(int &n){
+    n++;
+}
+
+int main(){
+    int a=3;
+    cout<<a<<endl;
+    increment(a);
+    cout<<a<<endl;
+    return 0;
+}
