60bb5acef1f47e1d6259a25450fdc254712f7ade
abhishek <abhishekmankame962@gmail.com>
2025-04-12 12:54:42 +0530

    code update

--- cp_cpp/luv/cpp_refrences.cpp
+++ cp_cpp/luv/cpp_refrences.cpp
@@ -3,6 +3,13 @@ using namespace std;
 
 void increment(int &n){
     n++;
+    // If parameter is passed by value in the function, then it will not change in the main function
+    // If the parameter is passed by reference in the function, then it will also change in the main function
+}
+// Note arrays kabhi bhi by values pass nahi hote, it will always pass by reference
+
+void func(string &s){
+    s="";
 }
 
 int main(){
@@ -10,5 +17,10 @@ int main(){
     cout<<a<<endl;
     increment(a);
     cout<<a<<endl;
+
+    string st="abhi";
+    cout<<st<<endl;
+    func(st);
+    cout<<st<<endl;
     return 0;
 }
