81150b2073cf3c07e7be49a0fd7d91ac4cadb569
abhishek <abhishekmankame962@gmail.com>
2025-04-13 17:29:32 +0530

    modular arithmetics

---
+++ cp_cpp/luv/print_answer_modulo.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*Modular Arithmetics
+--> (a+b)%M = ((a%M)+(b%M))%M
+--> (a*b)%M = ((a%M)*(b%M))%M
+--> (a-b)%M = ((a%M)-(b%M)+M)%M  // Here we added M to avoid the negative value
+
+
+*/
+
+
+int main(){
+
+
+    return 0;
+}
