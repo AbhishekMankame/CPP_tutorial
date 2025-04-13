efb4f2e0b39617babe4fba40af7b69e312a0317c
abhishek <abhishekmankame962@gmail.com>
2025-04-13 17:55:25 +0530

    factorial of number, print answer modulo 10^9+7

--- cp_cpp/luv/print_answer_modulo.cpp
+++ cp_cpp/luv/print_answer_modulo.cpp
@@ -5,13 +5,18 @@ using namespace std;
 --> (a+b)%M = ((a%M)+(b%M))%M
 --> (a*b)%M = ((a%M)*(b%M))%M
 --> (a-b)%M = ((a%M)-(b%M)+M)%M  // Here we added M to avoid the negative value
+--> (a/b)%M = ((a%M)*(b^-1)%M)%M // Here b^-1 is multiplicative inverse of b
 
+*/
 
+/* Given a number N. Print it's factorial. Print answer modulo 10^9+7.
+Constraints
+1<=N<=188
 */
 
 
 int main(){
-
+    int n;
 
     return 0;
 }
