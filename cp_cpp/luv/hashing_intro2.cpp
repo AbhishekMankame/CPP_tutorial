5ad7b027f0d18a14a471d31f033750a9b447009d
abhishek <abhishekmankame962@gmail.com>
2025-04-13 20:17:08 +0530

    code update using hashing

--- cp_cpp/luv/hashing_intro2.cpp
+++ cp_cpp/luv/hashing_intro2.cpp
@@ -1,10 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
+const int N = 1e7+10;
+int hsh[N];
 
 // Solving using hashing
 // Here we will use hash array to store the count, hash array ke har ek index mei uss element ka count store kar denge
 // For example, hash array ke 0th index mei, 0 ke count store karenge
 
 int main(){
-    
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+        hsh[a[i]]++;
+    }
+    int q;
+    cin>>q;
+    while(q--){
+        int x;
+        cin>>x;
+        cout<<hsh[x]<<endl;
+        
+    }
 }
