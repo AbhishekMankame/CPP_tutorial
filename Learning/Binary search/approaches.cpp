0d367ab90516c580ea35ed26cb4257c72f6f9e47
abhishek <abhishekmankame962@gmail.com>
2025-04-21 12:11:44 +0530

    binary search recursive approach

--- Learning/Binary search/approaches.cpp
+++ Learning/Binary search/approaches.cpp
@@ -16,3 +16,16 @@ int binarySearchIterative(int arr[], int n, int target){
     }
     return -1; // not found
 }
+
+int binarySearchRecursive(int arr[], int low, int high, int target){
+    if(low>high){
+        return -1;  // base case: not found
+    }
+    int mid = low + (high + low)/2;
+    if(arr[mid]==target)
+        return mid;
+    else if(arr[mid]<target)
+        return binarySearchRecursive(arr,mid+1,high,target);
+    else
+        return binarySearchRecursive(arr,low,mid-1,target);
+}
