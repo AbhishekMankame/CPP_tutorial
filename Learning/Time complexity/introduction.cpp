6b3236bc0a5a9fd8cd4e3b5f2434643de3edfb9e
abhishek <abhishekmankame962@gmail.com>
2025-04-21 16:48:39 +0530

    log n complexity details

--- Learning/Time complexity/introduction.cpp
+++ Learning/Time complexity/introduction.cpp
@@ -9,3 +9,16 @@ typedef long long int ll;
 int getFirstElement(int arr[]){
     return arr[0]; // Always one operation
 }
+
+// O(log n) - Logarithmic Time --> Input size reduces by half every step (e.g. binary search)
+
+int binarySearch(int arr[], int n, int target){
+    int left=0,right=n-1;
+    while(left<=right){
+        int mid=left+(right-left)/2;
+        if(arr[mid]==target) return mid;
+        else if(arr[mid]<target)    left = mid+1;
+        else right = mid-1;
+    }
+    return -1;
+}
