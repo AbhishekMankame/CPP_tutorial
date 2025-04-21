094acb659cb4154d00c4d96f8ecd6ccca92f94a0
abhishek <abhishekmankame962@gmail.com>
2025-04-21 16:43:13 +0530

    time complexity explanation

---
+++ Learning/Time complexity/introduction.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+// Time complexity: It describes how the runtime of an algorithm grows with respect to the input size n. 
+
+// Constant - O(1) --> Does not grow with input size
+
+int getFirstElement(int arr[]){
+    return arr[0]; // Always one operation
+}
