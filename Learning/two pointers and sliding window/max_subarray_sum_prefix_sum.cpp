9cc708f6dc89858b8c6626a66c5f8186f858075d
abhishek <abhishekmankame962@gmail.com>
2025-05-11 23:11:36 +0530

    prefix sum method

---
+++ Learning/two pointers and sliding window/max_subarray_sum_prefix_sum.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int largestSubarraySum(int arr[], int n){
+    // Prefix sums
+    int prefix[n] = {0};
+    prefix[0] = arr[0];
+    for(int i=1; i<n; i++){
+        prefix[i] = prefix[i-1] + arr[i];
+    }
+
+    int largest_sum = 0;
+    for(int i=0; i<n; i++){
+        for(int j=i;j<n;j++){
+            int subarraySum = prefix[j] - prefix[i-1];
+            largest_sum = max(largest_sum,subarraySum);
+        }
+    }
+
+    return largest_sum;
+}
