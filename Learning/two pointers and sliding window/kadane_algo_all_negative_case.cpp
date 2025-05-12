6d067efcbb56a35e89b1be6634c0b4e24538d682
abhishek <abhishekmankame962@gmail.com>
2025-05-12 07:28:57 +0530

    kadane algorithm special case update

---
+++ Learning/two pointers and sliding window/kadane_algo_all_negative_case.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+// Special case of Kadane Algorithm:
+// There is a possibility that all the element of array can be negative.
+// In that case we need to return the smallest negative number as the output
+
+int maxSubarray(vector<int> A){
+
+}
