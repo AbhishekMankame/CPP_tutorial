ac4d509b98c12adb728f504bc837d0c45db6f66f
abhishek <abhishekmankame962@gmail.com>
2025-05-12 06:16:05 +0530

    vector introduction

---
+++ Learning/vector_cpp/introduction.cpp
@@ -0,0 +1,6 @@
+// Vector
+/*
+- Vectors are sequence containers representing arrays that can change in size.
+- Just like arrays, vectors use contiguous storage locations for their elements, which means that their elements can also be accessed directly in O(1) time.
+- But unlike arrays, their size can change dynamically, with their storage being handled automatically by the container.
+*/
