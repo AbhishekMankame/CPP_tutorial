72991304ffe84c7dfe34ca3b856c6f8cfab987e4
abhishek <abhishekmankame962@gmail.com>
2025-05-12 06:31:50 +0530

    printing elements of vector

--- Learning/vector_cpp/introduction.cpp
+++ Learning/vector_cpp/introduction.cpp
@@ -17,8 +17,20 @@ using namespace std;
 int main(){
     vector<int> arr = {1, 2, 10, 12, 15};
 
-    // push_back()
+    // push_back() --> push_back function inserts at the end of the vector at O(1) time.
     arr.push_back(32);
+
+    // Print all the elements
+    for(int i = 0; i < arr.size(); i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+
+    // Let's use for each loop to print the elements of vector
+    for(int i : arr) {
+        cout<<i<<" ";
+    }
+    cout<<endl;
 
     // Size of the vector (No. of elements)
     cout<<arr.size()<<endl;
