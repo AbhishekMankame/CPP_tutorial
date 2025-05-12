0ab325d23f946b1c4e64b7222048e5c3f4496662
abhishek <abhishekmankame962@gmail.com>
2025-05-12 06:09:07 +0530

    kadane code

---
+++ Learning/two pointers and sliding window/kadane_algorithm.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int largestSubarraySum(int arr[], int n){
+    int cs=0;
+    int ls=0;
+    for(int i=0;i<n;i++){
+        cs = cs + arr[i];
+        if(cs<0)    cs=0;
+        ls = max(ls,cs);
+    }
+
+    return ls;
+}
+
+int main(){
+    int arr[] = {-2, 3, 4, -1, 5, -12, 6, 1, 3};
+    int n = sizeof(arr) / sizeof(int);
+    cout << largestSubarraySum(arr,n)<<endl;
+}
