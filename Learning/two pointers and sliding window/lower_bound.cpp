5acbff4805aa30babc3f5f97236d5d4fc94d1eab
abhishek <abhishekmankame962@gmail.com>
2025-05-12 07:48:00 +0530

    lower bound details update

---
+++ Learning/two pointers and sliding window/lower_bound.cpp
@@ -0,0 +1,15 @@
+/*Lower Bound
+Given an array of integers A (sorted) amd a integer Val.
+Implememt a function that takes A and Val as input parameters and returns the lower bound of Val. 
+Note: If Val is not present in array then Lower bound of a given integer means integer which is just smaller than given integer. 
+Otherwise Val itself is the answer.
+If Val is less than smallest element of array A then return -1 in that case. 
+
+Example1: 
+A = {-1, -1, 2, 3, 5} Val = 4
+Answer: 3
+Since 3 is just smaller than 4 in the array.
+
+Example 2:
+
+*/
