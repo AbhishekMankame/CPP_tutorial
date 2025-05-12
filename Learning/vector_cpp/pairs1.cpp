ab323121ef0d776cb1800edc517715cfbbc058b3
abhishek <abhishekmankame962@gmail.com>
2025-05-12 14:48:17 +0530

    pairs brute force code

--- Learning/vector_cpp/pairs1.cpp
+++ Learning/vector_cpp/pairs1.cpp
@@ -6,3 +6,22 @@ array = [10, 5, 2, 3, -6, 9, 11]
 S = 4
 Output: [10, -6]
 */
+
+// Let's solve with Brute force approach
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main(){
+    int arr[] = {10, 5, 2, 3, -6, 9, 11};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int s = 4;
+    for(int i=0; i<n;i++){
+        for(int j=i+1; j<n; j++){
+            if(arr[i] + arr[j] == s){
+                cout<<"["<<i<<","<<j<<"]";
+            }
+        }
+    }
+}
