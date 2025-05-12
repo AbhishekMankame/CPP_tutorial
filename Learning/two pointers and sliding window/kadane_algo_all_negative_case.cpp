a30108cd0a336ea1694002ba8185267b728efbf3
abhishek <abhishekmankame962@gmail.com>
2025-05-12 07:40:19 +0530

    code update

--- Learning/two pointers and sliding window/kadane_algo_all_negative_case.cpp
+++ Learning/two pointers and sliding window/kadane_algo_all_negative_case.cpp
@@ -39,3 +39,9 @@ int maxSubarray(vector<int> A){
 
     return ans;
 }
+
+int main(){
+    vector<int> arr = {-2, 3, 4, -1, 5, -12, 6, 1, 3};
+    //int n = sizeof(arr) / sizeof(int);
+    cout << maxSubarray(arr)<<endl;
+}
